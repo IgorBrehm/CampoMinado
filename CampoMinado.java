3b51c83f0e88f7df9363e99efa647fddb31fef9b
Guilherme <munaretto.guilherme@gmail.com>
2017-08-21 16:45:59 -0300

    Retirado o trecho de código que especificava a classe Mina e colocado em uma classe própria

--- CampoMinado.java
+++ CampoMinado.java
@@ -45,47 +45,6 @@ public class CampoMinado {
         System.out.println("E entre as coordenadas: "+coordVermin+", "+coordVermax+" no eixo vertical");
         */
     }
-    
-    /**
-    * Classe que especifica o objeto mina e seus métodos
-    * 
-    */
-    public class Mina {
-        public int v,h;
-        public Mina (){
-            v = -1;
-            h = -1;
-        }
-        public Mina(int ver, int hor){
-            v = ver;
-            h = hor;
-        }
-        /**
-         * Classe que distribui as minas pelo campo
-         * @param tamTerreno inteiro com o tamanho do terreno
-         * @param vetor  vetor contendo todas as minas que estarão no campo
-         */
-        public void distribuirMinas(int tamTerrenoh, int tamTerrenov, Mina[] vetor){
-            for(int k = 0; k < vetor.length; k++){
-                int aux = (int) (Math.random()*tamTerrenoh);
-                int auxb = (int) (Math.random()*tamTerrenov);
-                //int aux = in.next();
-                //int auxb = in.next();
-                vetor[k].h = aux;
-                vetor[k].v = auxb;
-            }
-        }
-        /**
-         * Método apenas para teste inicial, printa o vetor de minas com as suas  coordenadas
-         * @param vetor vetor contendo todas as minas que estarão no campo
-         */
-        public void printMinas(Mina[] vetor){
-            for(int k = 0; k < vetor.length; k++){
-                System.out.println("Mina numero: "+k);
-                System.out.println("Posicao hor: "+vetor[k].h+", Posicao ver: "+vetor[k].v);
-            }
-        }
-    }
 }
 
 
